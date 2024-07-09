03259b08bb4f91e17ab9b0279baaeacb14af74af
Julia Amaral <105997355+juliaamarall@users.noreply.github.com>
2024-07-08 23:16:45 -0300

    FOR,WHILE,DO WHILE

---
+++ javascript/arrays/script.js
@@ -0,0 +1,7 @@
+let nomes = ["julia", "suelen"];
+
+nomes.push("joão");
+
+console.log(nomes);
+
+//Os índices são essenciais para acessar e manipular elementos em um Array. Cada elemento possui uma posição específica, começando do 0 até o tamanho do Array menos um.
