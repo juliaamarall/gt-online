4d570fe6befb4b951283c2e32963707db43389c7
Julia Amaral <105997355+juliaamarall@users.noreply.github.com>
2024-07-09 23:50:12 -0300

    Normalizando terminações de linha

---
+++ react/react-aula1/src/App.jsx
@@ -0,0 +1,15 @@
+const Hello = () => {
+
+}
+
+
+
+//TODO COMPONENTE COMEÇA COM LETRA MAIUSCULA
+
+
+const App = () => {
+  //parte lógica fo componente
+  return(<h1>oieee</h1>)
+}
+
+export default App;
